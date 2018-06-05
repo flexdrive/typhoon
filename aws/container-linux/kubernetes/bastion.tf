3e3d01c510b42b818fc6548d94146a3d97f87aad
Tega McKinney <tega.mckinney@flexdrive.com>
2018-06-05 14:50:42 -0400

    Ummm....bastion should be public

--- aws/container-linux/kubernetes/bastion.tf
+++ aws/container-linux/kubernetes/bastion.tf
@@ -62,7 +62,7 @@ resource "aws_instance" "bastion" {
   user_data = "${data.ct_config.bastion_ign.rendered}"
 
   # network
-  subnet_id = "${element(aws_subnet.private.*.id, 0)}"
+  subnet_id = "${element(aws_subnet.public.*.id, 0)}"
 
   vpc_security_group_ids = [
     "${aws_security_group.bastion_external.id}",
