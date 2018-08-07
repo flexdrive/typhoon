14d1ab122d1df868b920ed2dd2f319a02060e0d3
Tega McKinney <tega.mckinney@flexdrive.com>
2018-08-07 16:35:56 -0400

    Ensure target_group_arns is a list

--- aws/container-linux/kubernetes/workers/workers.tf
+++ aws/container-linux/kubernetes/workers/workers.tf
@@ -15,11 +15,11 @@ resource "aws_autoscaling_group" "workers" {
   # template
   launch_configuration = "${aws_launch_configuration.worker.name}"
 
-  target_group_arns = "${concat("${var.addl_target_group_arns}",
+  target_group_arns = ["${concat("${var.addl_target_group_arns}",
     list(
       "${aws_lb_target_group.workers-http.id}",
       "${aws_lb_target_group.workers-https.id}"
-    ))}"
+    ))}"]
 
   lifecycle {
     # override the default destroy and replace update behavior
